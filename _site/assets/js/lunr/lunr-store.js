4bfb938ee71cd1390456b28dba9df90ad06347de
jasperpieterse <Jasperpieterse@protonmail.com>
2024-10-16 21:54:42 -0700

    YES!

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,10 +1,4 @@
 var store = [{
-        "title": "attempt",
-        "excerpt":"I’m on the web now babyyyy. And I’m not going anywhere.  ","categories": [],
-        "tags": [],
-        "url": "/work/",
-        "teaser": null
-      },{
         "title": "Bonjour",
         "excerpt":"I’m on the web now babyyyy  ","categories": [],
         "tags": [],
