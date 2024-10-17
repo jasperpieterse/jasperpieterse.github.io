b40aac2718f45dd17d8d05543cbcd23a2ea6d71c
jasperpieterse <Jasperpieterse@protonmail.com>
2024-10-16 21:53:11 -0700

    Show my post please

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,12 @@
 var store = [{
+        "title": "attempt",
+        "excerpt":"I’m on the web now babyyyy. And I’m not going anywhere.  ","categories": [],
+        "tags": [],
+        "url": "/work/",
+        "teaser": null
+      },{
         "title": "Bonjour",
-        "excerpt":"I’m Jasper, an MSc student and a poet  ","categories": [],
+        "excerpt":"I’m on the web now babyyyy  ","categories": [],
         "tags": [],
         "url": "/its-alive/",
         "teaser": null
