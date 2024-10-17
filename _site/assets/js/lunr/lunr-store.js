371f3118728aec53b0f599906de5eae61df3275f
jasperpieterse <Jasperpieterse@protonmail.com>
2024-10-16 21:44:42 -0700

    All good

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "Bonjour",
+        "excerpt":"I’m Jasper, an MSc student and a poet  ","categories": [],
+        "tags": [],
+        "url": "/its-alive/",
+        "teaser": null
+      }]
